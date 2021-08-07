62b7a5ab94b74c6d7f943c8927be303d2b815b1f
Jennifer Davila <pazjenni1331@gmail.com>
2021-08-07 18:29:50 -0500

    updated user-route.js file

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
-const userRoutes = require("./user-routes");
 
 router.use("/", homeRoutes);
-router.use("/user", userRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
