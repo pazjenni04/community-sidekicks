fad8e77fb148f8d65124d47f1d307e5105ef9d98
Jennifer Davila <pazjenni1331@gmail.com>
2021-08-07 18:22:03 -0500

    updated folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
-const homeRoutes = require("./home-route");
-const userRoutes = require("./user-route");
+const homeRoutes = require("./home-routes");
+const userRoutes = require("./user-routes");
 
 router.use("/", homeRoutes);
 router.use("/user", userRoutes);
