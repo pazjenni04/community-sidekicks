1f5a37cf832ed6bba80b5c70d6d12e6070437108
Kris Tina <kristyguerrero20@gmail.com>
2021-08-10 19:24:54 -0500

    finished user.js model

--- models/user.js
+++ models/user.js
@@ -1,37 +1,61 @@
 const { Model, DataTypes } = require("sequelize");
 const passport = require("passport");
 const sequelize = require("../config/connection");
-
+const user = require("./user");
 class user extends Model {
   checkPassword(login) {
-    return bcrypt.compareSync(login, this.password);
+    return passport.compareSync(login, this.password);
   }
 }
 
-user.init({
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+user.init(
+  {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
     },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8],
-    },
-  },
-});
 
-module.exports = user;
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await passport.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await passport.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = { user };
