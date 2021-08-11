7af23b9688676fe8a994af17334f737f4c38b0b9
Jennifer Davila <pazjenni1331@gmail.com>
2021-08-10 22:06:55 -0500

    Finished new account in frontend login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,13 @@
 //form to login to an existing account
-const form = {
-  email: document.querySelector("#userEmail").value.trim(),
-  password: document.querySelector("#userPassword").value.trim(),
-  submit: document.querySelector("#submitBtn"),
-};
 
 let submitForm = form.submit.addEventListener("click", (event) => {
   event.preventDefault();
+
+  const form = {
+    email: document.querySelector("#userEmail").value.trim(),
+    password: document.querySelector("#userPassword").value.trim(),
+    submit: document.querySelector("#submitBtn"),
+  };
 
   if (email && password) {
     const response = fetch("/api/user/login", {
@@ -24,3 +25,36 @@ let submitForm = form.submit.addEventListener("click", (event) => {
 });
 
 //new sign-up (account)
+const newAccount = async (event) => {
+  event.preventDefault();
+
+  const firstName = document.getElementById("firstName").value;
+  const lastName = docuemtn.getElementById("lastName").value;
+  const newEmail = document.getElementById("email-signup").value.trim();
+  const newPassword = document.getElementById("password-signup").value.trim();
+  const newUsername = document.getElementById("newUsername").value.trim();
+
+  if (firstName && lastName && newEmail && newPassword && newUsername) {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        newEmail,
+        newPassword,
+        newUsername,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up");
+    }
+  }
+};
+
+//event listener for signup
+
+document.getElementById("signupBtn").addEventListener("submit", newAccount);
