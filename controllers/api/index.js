891a429a5ef1384a9c7c32bd9e9defe3107dcd18
Jennifer Davila <pazjenni1331@gmail.com>
2021-08-12 21:23:39 -0500

    volunteer route works now - should run under api/volunteerhours

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
+const volunteerRoutes = require("./volunteer-routes");
 
 router.use("/user", userRoutes);
+router.use("/volunteerhours", volunteerRoutes);
 
 module.exports = router;
