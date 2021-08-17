a06f6f5cd4b1a93251f1ca4bb99540042028064b
Kris Tina <kristyguerrero20@gmail.com>
2021-08-17 18:30:11 -0500

    pulling from main branch

--- controllers/api/volunteer-routes.js
+++ controllers/api/volunteer-routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 
-const sequelize = require('../config/connection');
-const { VolunteerHours, user } = require('../models');
+const sequelize = require("../config/connection");
+const { VolunteerHours, user } = require("../models");
 
 // GET all volunteerhours for homepage
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const dbvolunteerhoursData = await VolunteerHours.findAll({
       // include: [
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
       VolunteerHours.get({ plain: true })
     );
 
-    res.render('volunteer-hours', {
+    res.render("volunteer-hours", {
       VolHours,
     });
   } catch (err) {
@@ -29,23 +29,19 @@ router.get('/', async (req, res) => {
 });
 
 // GET one gallery
-router.get('/VolunteerHours/:id', async (req, res) => {
+router.get("/VolunteerHours/:id", async (req, res) => {
   try {
     const dbvolunteehoursData = await VolunteerHours.findByPk(req.params.id, {
       include: [
         {
           model: VolunteerHours,
-          attributes: [
-            'id',
-            'name_organization',
-            'description',
-          ],
+          attributes: ["id", "name_organization", "description"],
         },
       ],
     });
 
     const volunteerhours = dbvolunteehoursData.get({ plain: true });
-    res.render('volunteerhours', { volunteerhours });
+    res.render("volunteerhours", { volunteerhours });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -53,19 +49,17 @@ router.get('/VolunteerHours/:id', async (req, res) => {
 });
 
 // GET one painting
-router.get('/user/:id', async (req, res) => {
+router.get("/user/:id", async (req, res) => {
   try {
     const dbuserData = await user.findByPk(req.params.id);
 
     const painting = dbuserData.get({ plain: true });
 
-    res.render('user', { user });
+    res.render("user", { user });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
-
-
 
 module.exports = router;
