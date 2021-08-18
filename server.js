d88762504dddbd547367d7430c4ef2264e80a894
Kris Tina <kristyguerrero20@gmail.com>
2021-08-17 20:43:08 -0500

    added statements to landing page

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Listening on port 8001!"));
+  app.listen(PORT, () => console.log("Listening on port " + PORT));
 });
